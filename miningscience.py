fec2b0941a2d9e2f5e1a1ed9a83f33421b48bcbd
LeslieTaimal <94822748+LeslieTaimal@users.noreply.github.com>
2022-02-22 11:29:17 -0500

    Leslie Taimal ha realizado el ejercicio 1

--- miningscience.py
+++ miningscience.py
@@ -1,28 +1,94 @@
-from Bio import Entrez
-import re
-
 def download_pubmed(keyword):
-    """Esta función sirve para la extracción y el conteo de los artículos que contienen una palabra en especial o conjunto de palabras , pues se puede definir si tienen que encontrarse en el título o en el abstract más comunmente"""
+    """Esta función sirve para la extracción y el conteo de los artículos que contienen una
+    palabra en especial o keyword llamado en la función, pues se puede definir si tienen que encontrarse en el
+    título la palabra o en el abstract más comunmunte, cabe destacar que esta función en el examen proporcionara
+    información acerca del número de artículos y el PMID del primer artículo """
+    from Bio import Entrez
+    import re
     Entrez.email = "leslie.taimal@est.ikiam.edu.ec"
-    hd1 = Entrez.read(Entrez.esearch(db="pubmed", 
-                        term=keyword,
+    x=Entrez.read(Entrez.esearch(db="pubmed",
+                        term= keyword,
                         usehistory="y"))
-    webenv = record["WebEnv"]
-    query_key = record["QueryKey"]
-    hd1 = Entrez.efetch(db="pubmed",
-                       retstart=0,
-                       rettype="medline",
-                       retmode="text",
-                       retmax=543, webenv=webenv, query_key=query_key)
-    out_hd1 = open("keyword.txt", "w")
-    data = hd1.read()
-    datos = re.sub(r'\n\s{6}', '', data)
-    return datos
-def mining_pubs(tipo):
-    """Esta función en cambio nos proporciona datos específicos del texto utilizando el parametro tipo porejemplo:
-    si el tipo es DP nos va a proporcionar el año de publicación del artículo 
-    si el tipo es AU no imprime el número de autores por PMID
-    si es AD en cambio nos ayuda con el conteo totales de cada país"""
-    pass
-    return
+    
+    webenv=x["WebEnv"]
+    query_key=x["QueryKey"]
+    hd1=Entrez.efetch(db="pubmed",
+                      rettype='medline',
+                      retmode="text",
+                      retstart=0,
+                      retmax=543, webenv=webenv, query_key=query_key)
+    out_hd1 = open(keyword+".txt", "w")
+    m=hd1.read()
+    out_hd1.write(m)
+    out_hd1.close()
+    hd1.close()
+    return m
+
+
+
+def mining_pubs(tipo, keyword):
+    """Esta funcion nos ayuda a la extraccion de datos según nos indique pued tipo hace realcion a DP,AU,AD, en cambio keyword hace relacion a las mismas palabras usadas en el ejercicio 1, pues de esta manera se realizo una función en general, cabe destacar tambien que:
+    DP recupera el año de publicación del artículo
+    AU recupera el número de autores por PMID
+    AD recupera el conteo de autores por país."""
+        
+
+    import csv
+    import re
+    import pandas as pd
+    from collections import Counter
+    with open(keyword+".txt", errors="ignore") as f: 
+        my_text = f.read() 
+    if tipo == "DP":
+        PMID = re.findall("PMID-\s\d{8}", my_text)
+        PMID = "".join(PMID)
+        PMID = PMID.split("PMID- ")
+        year = re.findall("DP\s{2}-\s(\d{4})", my_text)
+        pmid_year = pd.DataFrame()
+        pmid_year["PMID"] = PMID
+        pmid_year["Año de publicación"] = year
+        return (pmid_y)
+    elif tipo == "AU": 
+        PMID = re.findall("PMID- (\d*)", my_text) 
+        autors = texto.split("PMID- ")
+        autors.pop(0)
+        num_autors = []
+        for i in range(len(autores)):
+            num_total = re.findall("AU -", autores[i])
+            n = (len(num_total))
+            num_autors.append(n)
+        PMID_autors = pd.DataFrame()
+        PMID_autors["PMID"] = PMID 
+        pmid_autors["Numero de autores"] = num_autors
+        return (pmid_a)
+    elif tipo == "AD": 
+        text = re.sub(r" [A-Z]{1}\.","", my_text)
+        text = re.sub(r"Av\.","", my_text)
+        text = re.sub(r"Vic\.","", my_text)
+        text = re.sub(r"Tas\.","", my_text)
+        AD = text.split("AD  - ")
+        n_country = []
+        for i in range(len(AD)): 
+            country = re.findall("\S, ([A-Za-z]*)\.", AD[i])
+            if not country == []: 
+                if not len(country) >= 2:  
+                    if re.findall("^[A-Z]", country[0]): 
+                        n_country.append(country[0])
+        count=Counter(n_country)
+        result = {}
+        for clave in count:
+            valor = count[clave]
+            if valor != 1: 
+                result[clave] = valor 
+        count_pais = pd.DataFrame()
+        count_pais["pais"] = result.keys()
+        count_pais["numero de autores"] = result.values()
+        return (count_pais)
+    
+    
+    
+    
+
+            
+    
     
